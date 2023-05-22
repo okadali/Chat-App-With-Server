7eb6af924d9dbee0c589e01e0ecd6c3b877a0439
okadali <okadali@hotmail.com>
2023-05-22 23:41:10 +0300

    username input added

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 const socket = io('http://localhost:5000');
-socket.on("chat-message",(data) => {
-    console.log(data);
-})
+const loginScreen = document.getElementById('login-screen');
+const loginInput = document.getElementById('login-input');
+const loginButton = document.getElementById('login-button');
+const chatScreen = document.getElementById('chat-screen');
+
+    loginButton.addEventListener("click",() => {
+        if(loginInput.value.length > 6) {
+            socket.emit("login-info",loginInput.value);
+            loginScreen.style = "display: none;"
+            chatScreen.style = ""
+        }
+        else {
+            alert("Username Must Be Longer Than 6 Characters")
+        }
+        
+    })
+
+
+
+
+    // socket.on("chat-message",(data) => {
+    //     console.log('😉');
+    // })
+
+    // socket.on("listen-broadcast",(data) => {
+    //     console.log("BROADCAST MESSAGE: "+data);
+    // })
+
+    // messageForm.addEventListener('submit',e => {
+    //     e.preventDefault();
+    //     const message = messageInput.value
+    //     socket.emit('send-chat-message',message);
+    //     messageInput.value = '';
+    // })
--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const io = require("socket.io")(5000,{cors:{origin: "*"}})
+
+var allClients = {};
+
 io.on("connection", socket => {
-    socket.emit("chat-message","hello world");
+    allClients[socket.id] = "anonymous"
+    console.log(socket.id);
+
+    socket.on('login-info',(message) => {
+        allClients[socket.id] = message
+    })
+
+    socket.on('disconnect', () => {
+        delete allClients[socket.id]
+    })
+
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit("listen-broadcast",message.id);
+    })
 })
+
