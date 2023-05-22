9939dfc44d844b7a5e6811ab7e44186576df6d4c
okadali <okadali@hotmail.com>
2023-05-22 20:41:37 +0300

    cors error fix

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const io = require("socket.io")(5000)
+const io = require("socket.io")(5000,{cors:{origin: "*"}})
 io.on("connection", socket => {
     socket.emit("chat-message","hello world");
 })
