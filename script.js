58d655a34c1518802d4787b0149054fa34356cff
okadali <okadali@hotmail.com>
2023-05-23 14:29:46 +0300

    emotes added

--- script.js
+++ script.js
@@ -3,18 +3,62 @@ const loginScreen = document.getElementById('login-screen');
 const loginInput = document.getElementById('login-input');
 const loginButton = document.getElementById('login-button');
 const chatScreen = document.getElementById('chat-screen');
+const chatTextInput = document.getElementById('chat-text-input');
+const activeUsers = document.getElementById('active-users');
+const emotes = [...document.getElementsByTagName('td')];
 
-    loginButton.addEventListener("click",() => {
-        if(loginInput.value.length > 6) {
-            socket.emit("login-info",loginInput.value);
-            loginScreen.style = "display: none;"
-            chatScreen.style = ""
-        }
-        else {
-            alert("Username Must Be Longer Than 6 Characters")
-        }
-        
+
+class User {
+    constructor(id,username) {
+        this.id = id;
+        this.username = username;
+    }
+}
+
+emotes.forEach((item) => {
+    item.addEventListener('click', () => {
+        chatTextInput.value = chatTextInput.value + item.innerText;
     })
+})
+
+
+
+var onlineUsers = [];
+var selectedPersonToChat;
+
+loginButton.addEventListener("click", () => {
+    if(loginInput.value.length > 6) {
+        socket.emit("login-info", loginInput.value);
+        loginScreen.style = "display: none;";
+        chatScreen.style = "";
+    } 
+    else {
+        alert("Username Must Be Longer Than 6 Characters");
+    }
+});
+
+socket.on("get-users", (data) => {
+    let initialData = JSON.parse(data);
+    delete initialData[socket.id] 
+    onlineUsers = [];
+    for(const key in initialData) {
+        onlineUsers.push(new User(key,initialData[key]));
+    }
+    activeUsers.innerHTML = '';
+    onlineUsers.forEach((item) => {
+        let div = document.createElement("div");
+        div.addEventListener("click", () => {
+            selectedPersonToChat = item.id;
+            console.log(item.id);
+        })
+        let span = document.createElement("span");
+        span.textContent = item.username;
+        span.className = "active-users-item-span"
+        div.className = "active-users-item";
+        div.appendChild(span)
+        activeUsers.appendChild(div);
+    })
+}); 
 
 
 
--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const io = require("socket.io")(5000,{cors:{origin: "*"}})
 
 var allClients = {};
 
+const updateOnlineUsers = () => {
+    io.emit("get-users",JSON.stringify(allClients));
+}
+
 io.on("connection", socket => {
     allClients[socket.id] = "anonymous"
-    console.log(socket.id);
+    updateOnlineUsers();
 
     socket.on('login-info',(message) => {
         allClients[socket.id] = message
+        updateOnlineUsers();
     })
 
     socket.on('disconnect', () => {
         delete allClients[socket.id]
+        updateOnlineUsers();
     })
 
     socket.on('send-chat-message', message => {
